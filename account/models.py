1a3890876d6b3f9a3947efa38fafca94dc34e324
Amir Mashayekhi <ap4games2017@gmail.com>
2024-11-04 18:43:57 +0330

    implement register view and connect it with serializer

--- account/models.py
+++ account/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 # Create your models here.
 # class CustomUser(AbstractUser) :
-#     ema
+#     email
--- account/serializers.py
+++ account/serializers.py
@@ -8,7 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'password2', 'first_name', 'last_name')
+        fields = ('username', 'password', 'password2', 'first_name', 'last_name', 'email')
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
